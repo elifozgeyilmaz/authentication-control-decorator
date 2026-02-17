58fd2d13929298a94c294cc407ca5f0828e145a2
elifozgeyilmaz <119361791+elifozgeyilmaz@users.noreply.github.com>
2026-02-17 14:32:50 +0300

    Fix role check to support multiple roles

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 def require_role(required_role):
     def decorator(func):
         def wrapper(user, *args, **kwargs):
-            if user.role != required_role:
+            if user.roles != required_role:
                 raise PermissionError("Forbidden")
             return func(user, *args, **kwargs)
         return wrapper
